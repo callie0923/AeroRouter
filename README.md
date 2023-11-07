e5993d672173f53ade509fa3a448130e24f43245
MCampbell <cmvzjx@gmail.com>
2023-11-07 00:52:21 -0500

    First Commit
    Readme.md
    Small Routing Changes

--- README.md
+++ README.md
@@ -1,2 +1,52 @@
+
 # AeroRouter
- AeroRouter a Route generation tool.
+
+Overview:
+
+AeroRouter is a sophisticated tool designed to simplify the process of selecting random flight destinations based on user-defined categories of aircraft. This powerful utility harnesses a comprehensive database of global airport data to provide users with precise and realistic flight route suggestions, complete with essential flight planning information. Whether you are a pilot, aviation enthusiast, or an adventurous traveler, AeroRouter is your ultimate guide to thrilling aviation experiences.
+
+Features:
+
+Randomized Destination Selection: AeroRouter enables you to choose destinations at random, ensuring a fresh and exhilarating experience each time you use the tool.
+Aircraft Category Filters: Select your preferred category of aircraft, such as commercial airliners, general aviation, cargo planes, or military aircraft, and let AeroRouter suggest destinations that match your choice.
+Detailed Route Information: Each recommended route includes a wealth of information, including the route of flight, altitude recommendations, preferred category of aircraft, and more. This data is essential for precise flight planning and execution.
+Global Airport Data: AeroRouter is supported by a vast and up-to-date database of airports worldwide. With this tool, you can explore destinations across the globe, from major international hubs to remote airstrips.
+User-Friendly Interface: The user interface is intuitive and user-friendly, making it easy for both aviation experts and newcomers to navigate and utilize the tool effectively.
+Endless Exploration: With AeroRouter, you can embark on endless aviation adventures, discovering new places to fly and enhancing your flight simulation experiences.
+How to Use:
+
+Select the category of aircraft you wish to use for your flight, such as commercial, general aviation, cargo, or military.
+Click the "Generate Route" button to receive a randomly selected destination based on your chosen category.
+Explore the detailed information provided for your chosen route, including the recommended altitude, preferred aircraft category, and other essential details.
+Plan your flight and set off on an exciting journey to your newly selected destination.
+Getting Started:
+
+Clone or download the AeroRouter repository from GitHub.
+Follow the installation instructions in the README to set up the tool on your preferred platform.
+Immerse yourself in the boundless opportunities to discover new flight destinations and enrich your aviation experiences.
+Contributions and Issues:
+
+We welcome contributions, suggestions, and bug reports from the aviation community. If you encounter any issues or have ideas for improving AeroRouter, please do not hesitate to create an issue or submit a pull request on our GitHub repository.
+
+Embark on your aviation journey with AeroRouter. Experience the world of flight like never before. Bon voyage!
+
+## Authors
+
+- [@MCampbell](https://www.github.com/callie0923
+)
+
+
+## FAQ
+
+#### Can this be used for real world navigation?
+
+AeroRouter is expressly intended for entertainment and educational purposes only. It is not suitable or authorized for real-world aviation navigation, flight planning, or any activities that could affect air traffic or safety. Any such use is strictly prohibited, and the developers and contributors of AeroRouter shall not be held responsible for any consequences resulting from its misuse in real-world flight operations. Always adhere to proper aviation regulations, procedures, and certified navigation tools when engaging in real aviation activities.
+
+#### The program is not functioning as expected?
+
+Please keep in mind that AeroRouter is a new project developed during our free time and is not intended for sale. We acknowledge that there may be issues, and we welcome users to submit any problems or suggestions to our GitHub page for the project. Your feedback is highly valued, and it will help us improve and enhance AeroRouter for a better user experience. Thank you for your understanding and support in making this project better.
+
+
+## Acknowledgements
+
+ - [AeroAPI - IFR Route Analyzer ](https://www.flightaware.com/commercial/aeroapi/)
--- main.py
+++ main.py
@@ -1,21 +1,126 @@
-import tkinter as tk
+import random
+from geopy.distance import geodesic
+from json import load
+from math import atan2, degrees, radians, sin, cos
+import json
 
-# Create a GUI window
-window = tk.Tk()
-window.title("Flight Options")
-window.geometry("800x600")
+# Define a function to calculate initial compass bearing
+def calculate_initial_compass_bearing(pointA, pointB):
+    if (type(pointA) != tuple) or (type(pointB) != tuple):
+        raise TypeError("Only tuples are supported as arguments")
 
-# Create a label to display the output
-result_label = tk.Label(window, text="Click the button to display flight information.")
-result_label.pack()
+    lat1 = pointA[0]
+    lat2 = pointB[0]
+    lon1 = pointA[1]
+    lon2 = pointB[1]
 
-# Function to display flight options
-def display_flight_options():
-    result_label.config(text="Flight information displayed.")
+    if (lat1 == lat2) and (lon1 == lon2):
+        return 0
 
-# Create a button to generate flight options
-run_button = tk.Button(window, text="Generate Flight Options", command=display_flight_options)
-run_button.pack()
+    delta_lon = lon2 - lon1
 
-# Start the GUI main loop
-window.mainloop()
+    x = atan2(
+        sin(radians(delta_lon)) * cos(radians(lat2)),
+        cos(radians(lat1)) * sin(radians(lat2)) - sin(radians(lat1)) * cos(radians(lat2)) * cos(radians(delta_lon))
+    )
+
+    initial_bearing = degrees(x)
+    compass_bearing = (initial_bearing + 360) % 360
+
+    return compass_bearing
+
+# Load airport data from the JSON file
+with open('airports.json', 'r') as f:
+    airport_data = json.load(f)
+
+# Filter airports that belong to the United States
+us_airports = [airport for airport in airport_data if airport['country'] == 'United States']
+
+# Extract the airport codes and coordinates for US airports
+us_airport_coords = {airport['iata_code']: (float(airport['_geoloc']['lat']), float(airport['_geoloc']['lng'])) for airport in us_airports}
+
+# Load aircraft data from the JSON file
+with open('aircraft.json', 'r') as f:
+    aircraft_data = json.load(f)
+
+# List to store selected flight options
+flight_options = []
+
+# Number of flight options to generate
+num_flight_options = 3
+
+for _ in range(num_flight_options):
+    # Randomly select an aircraft category
+    selected_category = random.choice(list(aircraft_data.keys()))
+
+    # List to store selected legs for this set
+    legs = []
+    available_airports = list(us_airport_coords.keys())
+
+    # Generate three legs for this set
+    for _ in range(3):
+        # Randomly select a departure airport
+        departure = random.choice(available_airports)
+
+        # Randomly select an arrival airport
+        available_airports.remove(departure)
+        arrival = random.choice(available_airports)
+
+        # Calculate the distance and magnetic track for this leg
+        departure_coords = us_airport_coords[departure]
+        arrival_coords = us_airport_coords[arrival]
+        distance = geodesic(departure_coords, arrival_coords).nautical
+        track = calculate_initial_compass_bearing(departure_coords, arrival_coords)
+
+        # Calculate altitudes based on the distance and magnetic track
+        if distance < 25:
+            altitude = random.randint(3000, 5000)
+        elif 50 <= distance < 100:
+            altitude = random.randint(5000, 10000)
+        elif 100 <= distance < 500:
+            if 1 <= track <= 180:
+                altitude = random.choice(range(3000, 41000, 2000))  # Odd altitude
+            else:
+                altitude = random.choice(range(2000, 41000, 2000))  # Even altitude
+        elif 500 <= distance < 2000:
+            if 1 <= track <= 180:
+                altitude = random.choice(range(3000, 41000, 2000))  # Odd altitude
+            else:
+                altitude = random.choice(range(2000, 41000, 2000))  # Even altitude
+        else:
+            if 1 <= track <= 180:
+                altitude = random.choice(range(3000, 41000, 2000))  # Odd altitude (default)
+            else:
+                altitude = random.choice(range(2000, 41000, 2000))  # Even altitude (default)
+
+        # Check if the selected category is A, B, C, or D
+        if selected_category in ['Category A Aircraft', 'Category B Aircraft', 'Category C Aircraft', 'Category D Aircraft']:
+            # Apply distance restrictions for categories A-D
+            if 2000 <= distance <= 5000:
+                legs.append((departure, arrival, distance, track, altitude))
+            else:
+                # If the distance doesn't meet the requirements, switch to another category
+                while selected_category in ['Category A Aircraft', 'Category B Aircraft', 'Category C Aircraft', 'Category D Aircraft']:
+                    selected_category = random.choice(list(aircraft_data.keys()))
+                continue
+        else:
+            # Use other categories for routes that don't meet the requirements
+            legs.append((departure, arrival, distance, track, altitude))
+
+    # Append the selected legs for this set to the flight options list
+    flight_options.append((selected_category, legs))
+
+# Export the route as DCT for all legs
+route = "DCT"
+
+# Print the selected flight options for all sets
+for selected_category, legs in flight_options:
+    print(f"Selected Aircraft Category: {selected_category}")
+    for departure, arrival, distance, track, altitude in legs:
+        print(f"Departure Airport: {departure}")
+        print(f"Arrival Airport: {arrival}")
+        print(f"Route: {route}")
+        print(f"Altitude: FL{altitude // 1000:03d}")
+        print(f"Distance: {distance:.2f} nautical miles")
+        print(f"Track: {track:.2f} degrees")
+    print()
